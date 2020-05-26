844c3b3030da5514a63628cc4db7d2fae02b8877
Andrie <apdevries@gmail.com>
2020-05-26 10:29:14 +0000

    Add shiny example app

---
+++ inst/examples/shiny/app.R
@@ -0,0 +1,50 @@
+library(shiny)
+library(nomnoml)
+
+
+make_diagram <- function(name){
+  paste0("[hello]->[", name, "]")
+}
+
+ui <- fluidPage(
+  titlePanel(title = "Using 'nomnoml' in shiny"),
+  fluidRow(
+    box(
+      textInput("textbox", label = "Enter name:", value = "world")
+    )
+  ),
+  fluidRow(
+    box(
+      h2("Regular output"),
+      nomnomlOutput(outputId = "diagram_canvas", height = "100px")
+    )
+    ),
+    fluidRow(
+      box(
+        h2("SVG output"),
+        nomnomlOutput(outputId = "diagram_svg", height = "100px")
+      )
+  )
+)
+
+
+server <- function(input, output) {
+  
+  diagram <- reactive({
+    make_diagram(
+      name = input$textbox
+    )
+  })
+  
+  output$diagram_canvas <- renderNomnoml({
+    nomnoml(diagram(), svg = FALSE)
+  })
+  
+  output$diagram_svg <- renderNomnoml({
+    nomnoml(diagram(), svg = TRUE)
+  })
+  
+  
+}
+
+shinyApp(ui, server)
