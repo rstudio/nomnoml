033da2f68cc78de5bcdf64e1e02a98e32ab9effa
Andrie <apdevries@gmail.com>
2020-12-07 20:31:12 +0000

    Fix shinyloadtest in test suite

---
+++ inst/examples/shiny/tests/shinytest.R
@@ -0,0 +1,2 @@
+library(shinytest)
+shinytest::testApp("../")
--- tests/testthat/test-shiny.R
+++ tests/testthat/test-shiny.R
@@ -1,14 +1,18 @@
+
+
 library(shinytest)
-
-# Launch the target app (replace with the correct path)
-# recordTest("inst/examples/shiny")
-
 test_that("shiny", {
-  skip("need to troubleshoot appveyor")
+  # Launch the target app (replace with the correct path)
+  # recordTest("inst/examples/shiny")
+
+  skip_on_cran()
   
   # Use compareImages=FALSE because the expected image screenshots were created
   # on a Mac, and they will differ from screenshots taken on the CI platform,
   # which runs on Linux.
   appdir <- system.file(package = "nomnoml", "examples/shiny")
-  expect_pass(testApp(appdir, compareImages = FALSE))
+  expect_pass(
+    testApp(appdir, compareImages = FALSE)
+  )
 })
+
