e74f091dfd58610e783ca6775d4bad95aee278c9
Andrie <apdevries@gmail.com>
2020-12-09 13:27:54 +0000

    Clean up properly after knit tests

--- tests/testthat/test-knitr.R
+++ tests/testthat/test-knitr.R
@@ -2,17 +2,25 @@
 
 test_that("knitting works", {
   skip_on_cran()
-  
+
+  # make sure files don't exist  
   unlink("example_knitr_files", recursive = TRUE)
   unlink("example_knitr.knit.md")
+  
+  # render to .md
   z <- rmarkdown::render("example_knitr.Rmd", run_pandoc = FALSE, quiet = TRUE)
   
+  # check files exist
   expect_equivalent(z, "example_knitr.knit.md")
+  # expect_true(file.exists("example_knitr.html"))
   expect_true(file.exists("example_knitr_files/figure-html/one-1.png"))
   expect_true(file.exists("example_knitr_files/figure-html/two-2.png"))
   expect_true(file.size("example_knitr_files/figure-html/two-2.png") > 1000)
   expect_true(file.size("example_knitr_files/figure-html/two-2.png") > 1000)
   
+  # clean up
   unlink("example_knitr_files", recursive = TRUE)
+  unlink("_snaps", recursive = TRUE, force= TRUE)
   unlink("example_knitr.knit.md")
+  unlink("example_knitr.html")
 })
