035d1f457258f6a0caa5e651e76fb8896323c16f
Andrie de Vries <apdevries@gmail.com>
2022-02-16 17:06:10 +0000

    Minor fixes to test for testthat_3 conformance

--- tests/testthat/test-knitr.R
+++ tests/testthat/test-knitr.R
@@ -12,8 +12,7 @@ test_that("knitting works", {
   z <- rmarkdown::render("example_knitr.Rmd", run_pandoc = FALSE, quiet = TRUE)
   
   # check files exist
-  expect_equivalent(z, "example_knitr.knit.md", ignore_attr = TRUE)
-  # expect_true(file.exists("example_knitr.html"))
+  expect_equal(z, "example_knitr.knit.md", ignore_attr = TRUE)
   expect_true(file.exists("example_knitr_files/figure-html/one-1.png"))
   expect_true(file.exists("example_knitr_files/figure-html/two-2.png"))
   expect_true(file.size("example_knitr_files/figure-html/two-2.png") > 1000)
--- tests/testthat/test-shiny.R
+++ tests/testthat/test-shiny.R
@@ -6,14 +6,12 @@ test_that("shiny", {
   # recordTest("inst/examples/shiny")
 
   skip_on_cran()
+  skip_on_os("windows")
   testthat::skip_if_not(webshot::is_phantomjs_installed())
   
   # Only run the tests for code coverage on Github Actions
   
-  if (testthat:::on_ci()) {
-    skip_on_os("windows")
-  }
-
+  
   # Use compareImages=FALSE because the expected image screenshots were created
   # on a Mac, and they will differ from screenshots taken on the CI platform,
   # which runs on Linux.
